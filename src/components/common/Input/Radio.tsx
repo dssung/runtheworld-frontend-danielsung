57f5b3a1501cc80c24d8fa73b83798d98af3bdf8
Daniel Sung <dsung@dm.ai>
2020-05-07 19:52:21 -0700

    Radio error

--- src/components/common/Input/Radio.tsx
+++ src/components/common/Input/Radio.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 interface Props {
@@ -11,6 +11,19 @@ interface Props {
 const Radio: React.FC<Props> = ({
     label, name, values, handleChange
 }) => {
+    const [invalid, setInvalid] = useState(false);
+
+    const handleInvalid = (event: React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setInvalid(true);
+    }
+
+    const renderErrorMsg = () => {
+        let className = invalid ? 'error-msg' : 'error-msg hidden';
+        let message = 'Please choose one of the following:';
+        
+        return <p className={className}>{message}</p>
+    }
     
     const renderRadios = () => {
         return values.map(value => 
@@ -21,6 +34,7 @@ const Radio: React.FC<Props> = ({
                     onChange={handleChange} 
                     value={value}
                     required
+                    onInvalid={e => handleInvalid(e)}
                 />
                 <label> {value} </label>
             </>
@@ -29,9 +43,11 @@ const Radio: React.FC<Props> = ({
     
     return (
         <div className='radio'>
-            <p className='input-label'>
+            <p className='input-label radio-label'>
                 {label} <span className='required'>*</span>
             </p>
+            
+            {renderErrorMsg()}
             {renderRadios()}
         </div>
     );
